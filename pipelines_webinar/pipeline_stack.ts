62baab485ecd643ca62e8678e398615f25a86579
EC2 Default User <ec2-user@ip-172-31-61-119.us-west-2.compute.internal>
2022-03-04 13:35:02 +0000

    Deploy

--- pipelines_webinar/pipeline_stack.ts
+++ pipelines_webinar/pipeline_stack.ts
@@ -35,23 +35,23 @@ export class PipelineStackNew extends Stack {
         //
         const preProdApp = new WebServiceStage(this, 'Pre-Prod-New');
         const preProdStage = pipeline.addApplicationStage(preProdApp);
-        // const serviceUrl = pipeline.stackOutput(preProdApp.urlOutput);
+        const serviceUrl = pipeline.stackOutput(preProdApp.urlOutput);
 
-        //preProdStage.addActions(new pipelines.ShellScriptAction({
-         //   actionName: 'IntegrationTests',
-         //   runOrder: preProdStage.nextSequentialRunOrder(),
-          //  additionalArtifacts: [
-           //     sourceArtifact
-           // ],
-           // commands: [
-           //     'npm install',
-           //     'npm run build',
-           //     'npm run integration'
-           // ],
-           // useOutputs: {
-           //     SERVICE_URL: serviceUrl
-           //  }
-        //}));
+        preProdStage.addActions(new pipelines.ShellScriptAction({
+            actionName: 'IntegrationTests',
+            runOrder: preProdStage.nextSequentialRunOrder(),
+            additionalArtifacts: [
+                sourceArtifact
+            ],
+            commands: [
+                'npm install',
+                'npm run build',
+                'npm run integration'
+            ],
+            useOutputs: {
+                SERVICE_URL: serviceUrl
+             }
+        }));
 
         // Prod
         //
