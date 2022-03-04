1b97525e4b62e7e5b61d7b5c7b460b5cf4b10a2f
EC2 Default User <ec2-user@ip-172-31-61-119.us-west-2.compute.internal>
2022-03-04 11:56:17 +0000

    test

--- pipelines_webinar/pipeline_stack.ts
+++ pipelines_webinar/pipeline_stack.ts
@@ -33,25 +33,25 @@ export class PipelineStackNew extends Stack {
 
         // Pre-prod
         //
-        const preProdApp = new WebServiceStage(this, 'Pre-Prod-New');
-        const preProdStage = pipeline.addApplicationStage(preProdApp);
-        const serviceUrl = pipeline.stackOutput(preProdApp.urlOutput);
+       // const preProdApp = new WebServiceStage(this, 'Pre-Prod-New');
+        // const preProdStage = pipeline.addApplicationStage(preProdApp);
+        // const serviceUrl = pipeline.stackOutput(preProdApp.urlOutput);
 
-        preProdStage.addActions(new pipelines.ShellScriptAction({
-            actionName: 'IntegrationTests',
-            runOrder: preProdStage.nextSequentialRunOrder(),
-            additionalArtifacts: [
-                sourceArtifact
-            ],
-            commands: [
-                'npm install',
-                'npm run build',
-                'npm run integration'
-            ],
-            useOutputs: {
-                SERVICE_URL: serviceUrl
-            }
-        }));
+        //preProdStage.addActions(new pipelines.ShellScriptAction({
+         //   actionName: 'IntegrationTests',
+         //   runOrder: preProdStage.nextSequentialRunOrder(),
+          //  additionalArtifacts: [
+           //     sourceArtifact
+           // ],
+           // commands: [
+           //     'npm install',
+           //     'npm run build',
+           //     'npm run integration'
+           // ],
+           // useOutputs: {
+           //     SERVICE_URL: serviceUrl
+           //  }
+        //}));
 
         // Prod
         //
